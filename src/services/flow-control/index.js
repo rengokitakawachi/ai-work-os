9a2b77d98611ebf160437740185acbee5392bc81
rengokitakawachi <rengokitakawachi@gmail.com>
2026-04-13 16:00:03 +0900

    create flow-control exports for phase0 minimal foundation

---
+++ src/services/flow-control/index.js
@@ -0,0 +1,8 @@
+export { collectCandidates } from './candidate.js';
+export { normalizeCandidate, normalizeCandidates } from './normalize.js';
+export { evaluateCandidate, evaluateCandidates } from './rules.js';
+export {
+  buildTaskDraft,
+  buildPlacementDecision,
+  buildPlacementDecisions,
+} from './placement.js';
