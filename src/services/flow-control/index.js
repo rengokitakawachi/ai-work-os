1c19fbd297c1d0da55ebf36f22db66236cc58514
rengokitakawachi <rengokitakawachi@gmail.com>
2026-04-17 00:28:53 +0900

    export active operations source bundle adapter

--- src/services/flow-control/index.js
+++ src/services/flow-control/index.js
@@ -11,6 +11,7 @@ export {
   buildPlanSourceBundle,
   buildIssueSourceBundle,
   buildIssueRoutingSourceBundle,
+  buildActiveOperationsSourceBundle,
   buildNextOperationsSourceBundle,
   buildOperationsQueueSourceBundle,
   buildRollingSourceBundles,
